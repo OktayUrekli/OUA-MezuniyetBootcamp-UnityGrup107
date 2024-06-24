744aa499a69d942877e423641374b444c77dab76
Oktay Urekli <oktay.u.bm@gmail.com>
2024-06-24 17:37:39 +0300

    başka bir deneme daha

---
+++ Assets/denemeCS.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class denemeCS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("github denemesi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
