b5e38f409c9400e067ddb761737703ad69da676a
Okan Karasoglu <okan_karasoglu@hotmail.com>
2024-07-17 04:23:25 +0300

    Invisible walls and better spawn

--- Assets/Scripts/ShipSceneController.cs
+++ Assets/Scripts/ShipSceneController.cs
@@ -8,18 +8,18 @@ using UnityEditor;
 
 public class ShipSceneController : MonoBehaviour
 {
-    public SceneAsset mainMenuScene; // Drag and drop the main menu scene asset here in the editor
-    public SceneAsset gameScene; // Drag and drop the game scene asset here in the editor
+    public SceneAsset mainMenuScene; 
+    public SceneAsset gameScene; 
 
     public void LoadMainMenu()
     {
-        // Load scene by name using SceneAsset
+        
         SceneManager.LoadScene(mainMenuScene.name);
     }
 
     public void RetryGame()
     {
-        // Load scene by name using SceneAsset
+        
         SceneManager.LoadScene(gameScene.name);
     }
 }
